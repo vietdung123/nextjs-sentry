ffe51c975d37aca51225f7f87913c61d45cbb57c
vietdung123 <ngovietdung0112@gmail.com>
2022-10-14 10:39:49 +0700

    Init Sentry

---
+++ sentry.client.config.js
@@ -0,0 +1,14 @@
+// This file configures the initialization of Sentry on the browser.
+// The config you add here will be used whenever a page is visited.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: "https://cbd1fe1e7203488eab5d25db92aab4b7@o4503900646998016.ingest.sentry.io/4503964415950848",
+  
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
