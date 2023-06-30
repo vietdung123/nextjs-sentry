57657be4946b695baa4a889fcb194416c2ad4149
vietdung123 <ngovietdung0112@gmail.com>
2023-06-30 10:45:42 +0700

    update

---
+++ pages/hoc.js
@@ -0,0 +1,27 @@
+const WrapperComponent = ({ contacts }) => {
+    console.log('props WrapperComponent', contacts);
+  return <div>WrapperComponent</div>;
+};
+
+
+const withHOC = (key) => (WC) => {
+  // eslint-disable-next-line react/display-name
+  console.log('key', key);
+  return (props) => {
+    console.log({ props });
+    return <WC {...props} />
+  };
+};
+
+/* Static props can be passed down as function arguments */
+const ContactListWithLoadIndicator = withHOC("contacts")(WrapperComponent);
+
+const TestHOC = () => {
+  return (
+    <div>
+      <ContactListWithLoadIndicator contacts={"xxx"} />
+    </div>
+  );
+};
+
+export default TestHOC;
